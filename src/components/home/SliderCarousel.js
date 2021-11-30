a10ec336194cd6084ed86e1fdfa4fb3673112023
Ziya Sarı <ziya3435@gmail.com>
2021-12-01 00:25:10 +0300

    v4

--- src/components/home/SliderCarousel.js
+++ src/components/home/SliderCarousel.js
@@ -1,33 +1,23 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import sliderList from "../../data/sliderList";
 
 const SliderCarousel = () => {
   return (
     <Carousel indicators={false} variant="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/assets/img/cars/audi-a6.png"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/assets/img/cars/bmw-220.png"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      
+      {sliderList.map((slider) => (
+        <Carousel.Item key={slider.id}>
+          <img
+            className="d-block w-100"
+            src={slider.image}
+            alt={slider.title}
+          />
+          <Carousel.Caption>
+            <h3>{slider.title}</h3>
+            <p>{slider.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 };
---
+++ src/data/sliderList.js
@@ -0,0 +1,16 @@
+const sliderList = [
+  {
+    id: 1,
+    image: "/assets/img/cars/audi-a6.png",
+    title: "GET 25% OFF YOUR RENTAL",
+    description: "We have advantageous offers for your first rental",
+  },
+  {
+    id: 2,
+    image: "/assets/img/cars/volvo-s90.png",
+    title: "HYGIENIC VEHICLES WAIT YOU",
+    description: "You can safely drive our hygienic vehicles",
+  },
+];
+
+export default sliderList;
